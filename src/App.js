48bd8099b9cf378c6ce8ab39fcd372c11f26a940
rupeshghimirey <rupesh.ghimirey@gmail.com>
2021-11-03 14:29:54 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Pokemon from './components/Pokemon';
 import PokemonAxios from './components/PokemonAxios';
+import PokemonAxiosUseEffect from './components/PokemonAxiosUseEffect';
 function App() {
   return (
     <div className="container">
       {/* <Pokemon></Pokemon> */}
-      <PokemonAxios></PokemonAxios>
+      {/* <PokemonAxios></PokemonAxios> */}
+      <PokemonAxiosUseEffect></PokemonAxiosUseEffect>
     </div>
   );
 }
--- src/components/PokemonAxiosUseEffect.jsx
+++ src/components/PokemonAxiosUseEffect.jsx
@@ -1,13 +1,12 @@
 import axios from "axios";
-import react, { useState } from "react";
+import react, { useEffect, useState } from "react";
 import React from "react";
 
-const PokemonAxios = () => {
+const PokemonAxiosUseEffect = () => {
     const [listOfPokemons, setListOfPokemons] = useState([]);
 
-    const showPokemons = (e) => {
-        axios
-            .get("https://pokeapi.co/api/v2/pokemon")
+    useEffect(() => {
+        axios.get("https://pokeapi.co/api/v2/pokemon")
             .then((response) => {
                 console.log(response);
                 setListOfPokemons(response.data.results);
@@ -16,20 +15,16 @@ const PokemonAxios = () => {
             })
             .catch((err) => {
                 console.log(err);
-            });
-    };
+        });
+    }, []);
+
+    console.log("Wazzaaaa")
     return (
         <>
             <div className="container">
                 <h1 className="text-center text-info mt-3">
                     List of all the names of Pokemon Using Axios!
                 </h1>
-                <button
-                    onClick={showPokemons}
-                    className="btn btn-dark text-center col-lg-2 offset-lg-5 mt-2"
-                >
-                    Show All Pokemons
-                </button>
                 {listOfPokemons.map((eachPokemon, i) => {
                     return (
                         <div key={i}>
@@ -44,4 +39,4 @@ const PokemonAxios = () => {
     );
 };
 
-export default PokemonAxios;
+export default PokemonAxiosUseEffect;
